523b36b6edc17b68dda0160525d84f1091f3da7c
MohanadSinan <mohanad14141@hotmail.com>
2022-08-12 19:28:23 +0300

    new file:   .dockerignore 	new file:   .gitignore 	modified:   api/Dockerfile 	modified:   api/index.js 	modified:   docker-compose.yml

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Pool, Client } = require("pg");
 const app = express();
-const port = 3002;
+const port = 3001;
 
 const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
